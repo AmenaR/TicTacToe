6670290800ea2520c8583a3a07dee806077ea431
Amena Rafiq <amena.rafiq@avanade.com>
2024-04-18 11:01:19 +0100

    Add move history

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,58 +1,37 @@
-import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
 
-// export default App;
-
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i){
     //check if it already has been clicked so we don't overwrite, or someone has won already
-    if(squares[i] || calculateWinner(squares)){ 
+    if (calculateWinner(squares) || squares[i]) {
       return
     }
 
-    const nextSquares = squares.slice(); //make a copy for immutability
-
-    if(xIsNext){
-      nextSquares[i] = "X";
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
     } else {
-      nextSquares[i] = "O";
+      nextSquares[i] = 'O';
     }
-
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
   let status;
   if(winner){
-    status = "Winner: " + winner;
+    status = 'Winner: ' + winner;
   } else{
-    status = "Next Player: " + (xIsNext ? "X" : "O");
+    status = 'Next Player: ' + (xIsNext ? 'X' : 'O');
   }
 
   return (
@@ -78,19 +57,50 @@ export default function Board() {
   );
 }
 
-function Square({value, onSquareClick}) {
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else{
+      description = 'Restart game';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  })
+
   return (
-    <button 
-      className="square"
-      onClick={onSquareClick}
-    >
-      {value}
-    </button>
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
 
-function calculateWinner(squares){
-  const lines = [ //winning line combinations
+function calculateWinner(squares) {
+  const lines = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -98,9 +108,9 @@ function calculateWinner(squares){
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
-  for (let i=0; i<lines.length; i++){
+  for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
